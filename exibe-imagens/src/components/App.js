bbe13ef9ecd5794f14bfdc44cf4eadbc5632d290
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-13 22:26:50 -0300

    ap07(exibe-imagens): trabalha na secao 2.2
    - retomar terminando de fazer os imports da prime (explicar novamente)

---
+++ exibe-imagens/src/components/App.js
@@ -0,0 +1,12 @@
+// react arrow function component export: rafce
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Exibir uma lista de...</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ exibe-imagens/src/index.js
@@ -0,0 +1,11 @@
+// imr
+import React from 'react'
+// imrd
+import ReactDOM from 'react-dom'
+import 'primereact/resources/primereact.min.css'
+import App from './components/App'
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
