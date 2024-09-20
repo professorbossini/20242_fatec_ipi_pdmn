5819c2e9144cd406a6a96671734d320fc72c48ae
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-20 20:13:11 -0300

    ap07(exibe-imagens): pg 7 encerra o busca
    - retomar fazendo o Busca da forma mais moderna, como na documentacao

--- exibe-imagens/src/index.js
+++ exibe-imagens/src/index.js
@@ -2,7 +2,10 @@
 import React from 'react'
 // imrd
 import ReactDOM from 'react-dom'
-import 'primereact/resources/primereact.min.css'
+// import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/lara-light-blue/theme.css'
+import 'primeflex/primeflex.min.css'
+import 'primeicons/primeicons.css'
 import App from './components/App'
 
 ReactDOM.render(
